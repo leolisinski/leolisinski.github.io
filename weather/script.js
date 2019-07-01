62ee799e666720a161c5238fe076103dc0cb8e24
leolisinski <leo.lisinski@ga.ntig.se>
2019-07-01 15:58:53 +0200

    Update

--- weather/script.js
+++ weather/script.js
@@ -26,60 +26,85 @@ window.addEventListener('load', () => {
                     
                     if (WeatherSituationCode === 1) { 
                         WeatherSituation="Klart";
-                        }
+                        document.getElementById("icon1").style.visibility="visible";}
                     else if (WeatherSituationCode === 2) { 
-                        WeatherSituation="Lätt molnighet";}
+                        WeatherSituation="Lätt molnighet";
+                        document.getElementById("icon2").style.visibility="visible";}
                     else if (WeatherSituationCode === 3) { 
-                        WeatherSituation="Halvklart";}
+                        WeatherSituation="Halvklart";
+                        document.getElementById("icon3").style.visibility="visible";}
                     else if (WeatherSituationCode === 4) { 
-                        document.getElementById("icon4").style.visibility="visible";
-                        WeatherSituation="Molnigt";}
+                        WeatherSituation="Molnigt";
+                        document.getElementById("icon4").style.visibility="visible";}
                     else if (WeatherSituationCode === 5) { 
-                        WeatherSituation="Mycket moln";}
+                        WeatherSituation="Mycket moln";
+                        document.getElementById("icon5").style.visibility="visible";}
                     else if (WeatherSituationCode === 6) { 
-                        WeatherSituation="Mulet";}
+                        WeatherSituation="Mulet";
+                        document.getElementById("icon6").style.visibility="visible";}
                     else if (WeatherSituationCode === 7) { 
-                        WeatherSituation="Dimma";}
+                        WeatherSituation="Dimma";
+                        document.getElementById("icon7").style.visibility="visible";}
                     else if (WeatherSituationCode === 8) { 
-                        WeatherSituation="Lätt regnskur";}
+                        WeatherSituation="Lätt regnskur";
+                        document.getElementById("icon8").style.visibility="visible";}
                     else if (WeatherSituationCode === 9) { 
-                        WeatherSituation="Regnskur";}
+                        WeatherSituation="Regnskur";
+                        document.getElementById("icon9").style.visibility="visible";}
                     else if (WeatherSituationCode === 10) { 
-                        WeatherSituation="Kraftig regnskur";}
+                        WeatherSituation="Kraftig regnskur";
+                        document.getElementById("icon10").style.visibility="visible";}
                     else if (WeatherSituationCode === 11) { 
-                        WeatherSituation="Åskskur";}
+                        WeatherSituation="Åskskur";
+                        document.getElementById("icon11").style.visibility="visible";}
                     else if (WeatherSituationCode === 12) { 
-                        WeatherSituation="Lätt by av regn och snö";}
+                        WeatherSituation="Lätt by av regn och snö";
+                        document.getElementById("icon12").style.visibility="visible";}
                     else if (WeatherSituationCode === 13) { 
-                        WeatherSituation="By av regn och snö";}
+                        WeatherSituation="By av regn och snö";
+                        document.getElementById("icon13").style.visibility="visible";}
                     else if (WeatherSituationCode === 14) { 
-                        WeatherSituation="Kraftig by av regn och snö";}
+                        WeatherSituation="Kraftig by av regn och snö";
+                        document.getElementById("icon14").style.visibility="visible";}
                     else if (WeatherSituationCode === 15) { 
-                        WeatherSituation="Lätt snöby";}
+                        WeatherSituation="Lätt snöby";
+                        document.getElementById("icon15").style.visibility="visible";}
                     else if (WeatherSituationCode === 16) { 
-                        WeatherSituation="Snöby";}
+                        WeatherSituation="Snöby";
+                        document.getElementById("icon16").style.visibility="visible";}
                     else if (WeatherSituationCode === 17) { 
-                        WeatherSituation="Kraftig snöby";}
+                        WeatherSituation="Kraftig snöby";
+                        document.getElementById("icon17").style.visibility="visible";}
                     else if (WeatherSituationCode === 18) { 
-                        WeatherSituation="Lätt regn";}
+                        WeatherSituation="Lätt regn";
+                        document.getElementById("icon18").style.visibility="visible";}
                     else if (WeatherSituationCode === 19) { 
-                        WeatherSituation="Regn";}
+                        WeatherSituation="Regn";
+                        document.getElementById("icon19").style.visibility="visible";}
                     else if (WeatherSituationCode === 20) { 
-                        WeatherSituation="Kraftigt regn";}
+                        WeatherSituation="Kraftigt regn";
+                        document.getElementById("icon20").style.visibility="visible";}
                     else if (WeatherSituationCode === 21) { 
-                        WeatherSituation="Åska";}
+                        WeatherSituation="Åska";
+                        document.getElementById("icon21").style.visibility="visible";}
                     else if (WeatherSituationCode === 22) { 
-                        WeatherSituation="Lätt snöblandat regn";}
+                        WeatherSituation="Lätt snöblandat regn";
+                        document.getElementById("icon22").style.visibility="visible";}
                     else if (WeatherSituationCode === 23) { 
-                        WeatherSituation="Snöblandat regn";}
+                        WeatherSituation="Snöblandat regn";
+                        document.getElementById("icon23").style.visibility="visible";}
                     else if (WeatherSituationCode === 24) { 
-                        WeatherSituation="Kraftigt snöblandat regn";}
+                        WeatherSituation="Kraftigt snöblandat regn";
+                        document.getElementById("icon24").style.visibility="visible";}
                     else if (WeatherSituationCode === 25) { 
-                        WeatherSituation="Lätt snöfall";}
+                        WeatherSituation="Lätt snöfall";
+                        document.getElementById("icon25").style.visibility="visible";}
                     else if (WeatherSituationCode === 26) { 
-                        WeatherSituation="Snöfall";}
+                        WeatherSituation="Snöfall";
+                        document.getElementById("icon26").style.visibility="visible";}
                     else if (WeatherSituationCode === 27) { 
-                        WeatherSituation="Ymnigt snöfall";}
+                        WeatherSituation="Ymnigt snöfall";
+                        document.getElementById("icon27").style.visibility="visible";}
                     else { 
                         WeatherSituation="Saknar data för sammanfattning";}
 
