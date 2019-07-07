14f3d8941e9fc0d28542f46f597fd9ce5643e4c5
Dator <leo.lisinski@ga.ntig.se>
2019-07-07 15:26:28 +0200

    update

--- tetris/audio.js
+++ tetris/audio.js
@@ -1,17 +1,41 @@
 class AudioController {
     constructor() 
     {
-        this.bgMusic = new Audio('audio/tetris.mp3');
-        this.bgMusic.volume = 0.5; 
+        this.bgMusic = new Audio('audio/443128__sirkoto51__boss-battle-loop-3.wav');
+        this.flipSound = new Audio('audio/rotate.mp3');
+        this.rowSound = new Audio('audio/row.wav');
+        this.dropSound = new Audio('audio/drop.wav');
+        this.bgMusic.volume = 0.2; 
         this.bgMusic.loop = true;
+        this.flipSound.volume = 0;
+        this.rowSound.volume = 0;
+        this.dropSound.volume = 0;
     }
-    startMusic() 
+    startSound() 
     {
         this.bgMusic.play();
+        this.rowSound.volume = 0.5;
+        this.flipSound.volume = 0.5;
+        this.dropSound.volume = 0.5;
     }
-    stopMusic() 
+    stopSound() 
     {
         this.bgMusic.pause();
         this.bgMusic.currentTime = 0;
+        this.rowSound.volume = 0;
+        this.flipSound.volume = 0;
+        this.dropSound.volume = 0;
+    }
+    flip() 
+    {
+        this.flipSound.play();
+    }
+    row() 
+    {
+        this.rowSound.play();
+    }
+    drop()
+    {
+        this.dropSound.play();
     }
 }
--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,47 +1,4 @@
-class AudioController {
-    constructor() 
-    {
-        this.bgMusic = new Audio('audio/tetris.mp3');
-        this.flipSound = new Audio('audio/rotate.mp3');
-        this.rowSound = new Audio('audio/row.wav');
-        this.dropSound = new Audio('audio/drop.wav');
-        this.bgMusic.volume = 0.2; 
-        this.bgMusic.loop = true;
-        this.flipSound.volume = 0;
-        this.rowSound.volume = 0;
-        this.dropSound.volume = 0;
-    }
-    startSound() 
-    {
-        this.bgMusic.play();
-        this.rowSound.volume = 0.5;
-        this.flipSound.volume = 0.5;
-        this.dropSound.volume = 0.5;
-    }
-    stopSound() 
-    {
-        this.bgMusic.pause();
-        this.bgMusic.currentTime = 0;
-        this.rowSound.volume = 0;
-        this.flipSound.volume = 0;
-        this.dropSound.volume = 0;
-    }
-    flip() 
-    {
-        this.flipSound.play();
-    }
-    row() 
-    {
-        this.rowSound.play();
-    }
-    drop()
-    {
-        this.dropSound.play();
-    }
-}
-
 const audioController = new AudioController;
-
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 let pieceNumber = 0;
