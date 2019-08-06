25881c464a85764a11b9dc5754d101aed3aff76e
leolisinski <leo.lisinski@ga.ntig.se>
2019-08-06 16:22:11 +0200

    update with sound

--- kronan/audio.js
+++
@@ -1,9 +0,0 @@
-class Ljud {
-
-    constructor() {
-        
-    }
-
-
-
-}
--- kronan/game.js
+++ kronan/game.js
@@ -1,6 +1,27 @@
 "use strict";
 
-var c1 = []; var c2 = []; var c3 = []; var c4 = []; var c5 = []; var c6 = []; var c7 = []; var c8 = []; var c9 = []; var c10 = []; var c11 = []; var c12 = []; var c13 = []; var c14 = [];
+class Ljud {
+    constructor() {
+        this.bgMusic = new Audio('audio/346454__airwolf89__calm-game-music-1.mp3');
+        this.cardSound = new Audio('audio/240776__f4ngy__card-flip.wav');
+        this.bgMusic.volume = 0.5; 
+        this.bgMusic.loop = true;
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
+    stopMusic() {
+        this.bgMusic.pause();
+        this.bgMusic.currentTime = 0;
+    }
+    playCardSound() {
+        this.cardSound.play();
+    }
+}
+
+const audiocontroller = new Ljud()
+
+var audioOn = false
 
 function c(pileNr) {
     return eval(`c${pileNr}`)
@@ -22,7 +43,9 @@ function clickListener() {
                     stopListen = true
                     document.getElementById(`c${clickedCards[0]}`).classList.remove('selected')
                     document.getElementById(`c${clickedCards[1]}`).classList.remove('selected')
+                    if (audioOn) {audiocontroller.playCardSound()}
                     moveCard(clickedCards)
+                    
             }
         })
     }
@@ -40,6 +63,7 @@ function moveCard(clickedCards) {
 
 function Reset() {
     var kortlek = new Deck()
+    window.c1 = []; window.c2 = []; window.c3 = []; window.c4 = []; window.c5 = []; window.c6 = []; window.c7 = []; window.c8 = []; window.c9 = []; window.c10 = []; window.c11 = []; window.c12 = []; window.c13 = []; window.c14 = [];
     kortlek.clear_deck()
     kortlek.generate_deck()
     kortlek.shuffle()
@@ -95,5 +119,16 @@ document.getElementById("button_restart").addEventListener("click", () => {
 })
 
 document.getElementById("button_music").addEventListener("click", () => {
-    console.log("music")
+    if (audioOn) {
+        audiocontroller.stopMusic()
+        document.getElementById('button_music').innerHTML='Ljud: av'
+        document.getElementById('button_music').style.background='white'
+        audioOn = false
+    }
+    else {
+    audiocontroller.startMusic()
+    document.getElementById('button_music').innerHTML='Ljud: på'
+    document.getElementById('button_music').style.background='lightgreen'
+    audioOn = true}
+    
 })
