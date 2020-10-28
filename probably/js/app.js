c38e1d6750258a605e128b0e7949c9fe9a5b075a
Leo Lisinski <leo.lisinski@learnet.se>
2020-10-28 14:48:14 +0100

    update

--- probably/js/app.js
+++ probably/js/app.js
@@ -1,12 +1,16 @@
 const dices = document.getElementsByClassName('dice')
 
+function changeDiceFace(diceIndex, newFace) {
+    var dice = dices[diceIndex]
+    dice.src = dice.src.slice(0, dice.src.length - 5) + `${newFace}` + ".png"
+}
 
-function rollDice(diceIndex) {
+function rollDiceAnimated(diceIndex, time) {
     var dice = dices[diceIndex] 
     var i = 0
     var direction = Math.floor(Math.random() * 4)
     var diceFace = Math.floor(Math.random() * 6) + 1
-    while (i <= 10) { 
+    while (i <= time) { 
 
             setTimeout(() => {
                 let tempDir = Math.floor(Math.random() * 4)
@@ -42,9 +46,32 @@ function rollDice(diceIndex) {
         }
     }
 
-function rollAllDices() {
+function rollAllOneTime() {
+    var result = []
+    for (let i = 0; i < dices.length; i ++) {
+        var roll = Math.floor(Math.random() * 6) + 1
+        result.push(roll)
+        changeDiceFace(i, roll)
+    }
+    return result
+}   
+
+function rollAllManyTimes(times, milliSecondsPerRoll) {
+    for (let i = 0; i < times; i++) {
+        setTimeout(() => rollAllOneTime(), milliSecondsPerRoll*i)
+    }
+}
+
+function allSame(result) {
+    for (let i = 1; i < result.length; i++) {
+        if (result[i] != result[0]) {return false}
+    }
+    return true
+}
+
+function rollAllDicesAnimated(time) {
 for (let i = 0; i < dices.length; i++) {
-    rollDice(i)
+    rollDiceAnimated(i, time)
 }}
 
 function initialize() {
@@ -52,7 +79,41 @@ for (let i = 0; i < dices.length; i++) {
     let firstDiceFace = Math.floor(Math.random() * 6) + 1
     let dice = dices[i]
     dice.src = dice.src.slice(0, dice.src.length - 5) + `${firstDiceFace}` + ".png"
-    dice.addEventListener('click', () => rollAllDices())
+    dice.addEventListener('click', (event) => rollDiceAnimated(event.target.id,10)
+    )
+    dice.addEventListener('dblclick', () => rollAllDicesAnimated(10))
 }}
 
 initialize()
+
+function rollDice() { 
+    return Math.floor(Math.random() * 6) + 1
+}
+
+function rollAndTryToGetAllSame(iterations) {
+    var numberOfAllSame = 0
+    for (let i = 1; i <= iterations; i++) {
+        var diceFace = rollDice()
+        var allSame = true
+        for (let i = 1; i < dices.length; i++) {
+            if (rollDice() != diceFace) {allSame = false}
+        }
+        if (allSame) {numberOfAllSame += 1}
+    }
+    return numberOfAllSame
+}
+
+function rollUntilAllSame() {
+    allNotSame = true
+    iteration = 0
+    while (allNotSame) {
+        iteration += 1
+        var diceFace = rollDice()
+        var allSame = true
+        for (let i = 1; i < dices.length; i++) {
+            if (rollDice() != diceFace) {allSame = false}
+        }
+        if (allSame) {allNotSame = false}
+    }
+    return iteration
+}
