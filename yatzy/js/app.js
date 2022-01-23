1b304034dcb1c9440520bcaee0cb0449c8584f6a
leolisinski <leosmacbook@MacBook-Pro-som-tillhor-Leo.local>
2022-01-23 19:31:06 +0100

    update

--- yatzy/js/app.js
+++ yatzy/js/app.js
@@ -1,11 +1,25 @@
 const dices = document.getElementsByClassName('dice')
 const rollCounter = document.getElementById('counterRolls')
 const twoCounter = document.getElementById('counterTwo')
+const twoPairCounter = document.getElementById('counterTwoPair')
 const threeCounter = document.getElementById('counterThree')
 const fourCounter = document.getElementById('counterFour')
+const fullHouseCounter = document.getElementById('counterFullHouse')
+const smallStraightCounter = document.getElementById('counterSmallStraight')
+const largeStraightCounter = document.getElementById('counterLargeStraight')
 const fiveCounter = document.getElementById('counterFive')
-const button = document.getElementById('button')
+const noneCounter = document.getElementById('counterNone')
 const inputField = document.getElementById('input_box')
+
+const relativeTwoSpan = document.getElementById('relativeTwo')
+const relativeTwoPairSpan = document.getElementById('relativeTwoPair')
+const relativeThreeSpan = document.getElementById('relativeThree')
+const relativeFourSpan = document.getElementById('relativeFour')
+const relativeFullHouseSpan = document.getElementById('relativeFullHouse')
+const relativeSmallStraightSpan = document.getElementById('relativeSmallStraight')
+const relativeLargeStraightSpan = document.getElementById('relativeLargeStraight')
+const relativeFiveSpan = document.getElementById('relativeFive')
+const relativeNoneSpan = document.getElementById('relativeNone')
 
 inputField.addEventListener('click', () => {
     if (inputField.value == "Antal slag + ENTER")
@@ -24,55 +38,158 @@ document.addEventListener('click', (event) => {
         inputField.style.fontSize = "15px"
         inputField.value = "Antal slag + ENTER"
     }
-    if (event.target.id[0] == "b") {
-        rollAllFastManyTimesUntilNrOfSame(event.target.id.slice(1,event.target.id.length))        
-    }
 })
 
 
 var rollValue = 0
 var twoValue = 0
+var twoPairValue = 0
 var threeValue = 0
 var fourValue = 0
+var fullHouseValue = 0
+var smallStraightValue = 0
+var largeStraightValue = 0
 var fiveValue = 0
+var noneValue = 0
+
+var relativeTwoValue = 0
+var relativeTwoPairValue = 0
+var relativeThreeValue = 0
+var relativeFourValue = 0
+var relativeFullHouseValue = 0
+var relativeSmallStraightValue = 0
+var relativeLargeStraightValue = 0
+var relativeFiveValue = 0
+var relativeNoneValue = 0
+
 
 function updateValueAndHTML(identifier, value) {
     switch (identifier) {
-        case 1:
+        case "r":
             rollValue = value
             rollCounter.innerHTML = `${value}`
             break
-        case 2:
+        case "2":
             twoValue = value
+            if (rollValue != 0) {
+                relativeTwoValue = Math.round(((twoValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeTwoValue = 0
+            }
+            relativeTwoSpan.innerHTML = `${relativeTwoValue}`
             twoCounter.innerHTML = `${value}`
+            
             break
-        case 3:
+        case "2p":
+            twoPairValue = value
+            if (rollValue != 0) {
+                relativeTwoPairValue = Math.round(((twoPairValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeTwoPairValue = 0
+            }
+            twoPairCounter.innerHTML = `${value}`
+            relativeTwoPairSpan.innerHTML = `${relativeTwoPairValue}`
+            break
+        case "3":
             threeValue = value
+            if (rollValue != 0) {
+                relativeThreeValue = Math.round(((threeValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeThreeValue = 0
+            }
             threeCounter.innerHTML = `${value}`
+            relativeThreeSpan.innerHTML = `${relativeThreeValue}`
             break
-        case 4:
+        case "4":
             fourValue = value
+            if (rollValue != 0) {
+                relativeFourValue = Math.round(((fourValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeFourValue = 0
+            }
             fourCounter.innerHTML = `${value}`
+            relativeFourSpan.innerHTML = `${relativeFourValue}`
             break
-        case 5:
+        case "fh":
+            fullHouseValue = value
+            if (rollValue != 0) {
+                relativeFullHouseValue = Math.round(((fullHouseValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeFullHouseValue = 0
+            }
+            fullHouseCounter.innerHTML = `${value}`
+            relativeFullHouseSpan.innerHTML = `${relativeFullHouseValue}`
+            break
+        case "ss":
+            smallStraightValue = value
+            if (rollValue != 0) {
+                relativeSmallStraightValue = Math.round(((smallStraightValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeSmallStraightValue = 0
+            }
+            smallStraightCounter.innerHTML = `${value}`
+            relativeSmallStraightSpan.innerHTML = `${relativeSmallStraightValue}`
+            break
+        case "ls":
+            largeStraightValue = value
+            if (rollValue != 0) {
+                relativeLargeStraightValue = Math.round(((largeStraightValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeLargeStraightValue = 0
+            }
+            largeStraightCounter.innerHTML = `${value}`
+            relativeLargeStraightSpan.innerHTML = `${relativeLargeStraightValue}`
+            break
+        case "5":
             fiveValue = value
+            if (rollValue != 0) {
+                relativeFiveValue = Math.round(((fiveValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeFiveValue = 0
+            }
             fiveCounter.innerHTML = `${value}`
+            relativeFiveSpan.innerHTML = `${relativeFiveValue}`
+            break
+        case "n":
+            noneValue = value
+            if (rollValue != 0) {
+                relativeNoneValue = Math.round(((noneValue / rollValue) * 100) * 100) / 100
+            }
+            else {
+                relativeNoneValue = 0
+            }
+            noneCounter.innerHTML = `${value}`
+            relativeNoneSpan.innerHTML = `${relativeNoneValue}`
             break
     }
 }
 
 function clearValuesAndHTML() {
-    for (let i = 1; i <= 5; i++) {
-        updateValueAndHTML(i, 0)
+    var identifiers = ["r", "2", "2p", "3", "4", "fh", "ss", "ls", "5", "n"]
+    for (let i = 0; i < identifiers.length; i++) {
+        updateValueAndHTML(identifiers[i], 0)
     }
 }
 
 function updateHTMLfromValues() {
-    updateValueAndHTML(1, rollValue)
-    updateValueAndHTML(2, twoValue)
-    updateValueAndHTML(3, threeValue)
-    updateValueAndHTML(4, fourValue)
-    updateValueAndHTML(5, fiveValue)
+    updateValueAndHTML("r", rollValue)
+    updateValueAndHTML("2", twoValue)
+    updateValueAndHTML("2p", twoPairValue)
+    updateValueAndHTML("3", threeValue)
+    updateValueAndHTML("4", fourValue)
+    updateValueAndHTML("fh", fullHouseValue)
+    updateValueAndHTML("ss", smallStraightValue)
+    updateValueAndHTML("ls", largeStraightValue)
+    updateValueAndHTML("5", fiveValue)
+    updateValueAndHTML("n", noneValue)
 }
 
 
@@ -80,47 +197,6 @@ function changeDiceFace(diceIndex, newFace) {
     var dice = dices[diceIndex]
     dice.src = dice.src.slice(0, dice.src.length - 5) + `${newFace}` + ".png"
 }
-
-function rollDiceAnimated(diceIndex, time) {
-    clearValuesAndHTML()
-    var dice = dices[diceIndex] 
-    var i = 0
-    var direction = Math.floor(Math.random() * 4)
-    var diceFace = Math.floor(Math.random() * 6) + 1
-    while (i <= time) { 
-            setTimeout(() => {
-                let tempDir = Math.floor(Math.random() * 4)
-                while (tempDir == direction) {tempDir = Math.floor(Math.random() * 4)}
-                direction = tempDir
-                let tempFace = Math.floor(Math.random() * 6) + 1 
-                while (tempFace == diceFace) {tempFace = Math.floor(Math.random() * 6) + 1}
-                diceFace = tempFace
-                dice.src = dice.src.slice(0, dice.src.length - 5) + `${diceFace}` + ".png"
-                switch (direction) {
-                    case 0: 
-                        dice.style.top = "5px"
-                    break
-                    case 1: 
-                    dice.style.left = "5px"
-                    break
-                    case 2: 
-                    dice.style.left = "-5px"
-                    break
-                    case 3: 
-                    dice.style.top = "-5px"
-                    break
-                }
-            }, 100 + 200*i)
-
-            setTimeout(() => {
-                dice.style.top = "0px"
-                dice.style.left = "0px"
-            }, 200 + 200*i)
-
-            i += 1
-
-        }
-    }
 
 function rollAllOneTime() {
     var result = []
@@ -139,58 +215,39 @@ function rollAllFastNrOfTimes(number) {
     var interval = setInterval(() => {
         rollValue += 1
         result = rollAllOneTime()
-        switch(nrOfSame(result)) {
-            case 2:
+        switch(outcome(result)) {
+            case "2":
                 twoValue += 1
                 break
-            case 3: 
+            case "2p":
+                twoPairValue += 1
+                break
+            case "3": 
                 threeValue += 1
                 break
-            case 4:
+            case "4":
                 fourValue += 1
                 break
-            case 5:
+            case "fh":
+                fullHouseValue += 1
+                break
+            case "ss":
+                smallStraightValue += 1
+                break
+            case "ls":
+                largeStraightValue += 1
+                break
+            case "5":
                 fiveValue += 1
                 break
+            case "n":
+                noneValue += 1
+            break
         } 
         updateHTMLfromValues()
         if (i == number) {clearInterval(interval)}
         i += 1
 },1)
-}
-
-function rollAllFastManyTimesUntilNrOfSame(number) {
-    singleRoll = false
-    clearValuesAndHTML()
-    var interval = setInterval(() => {
-        result = rollAllOneTime()
-        switch(nrOfSame(result)) {
-            case 2:
-                twoValue += 1
-                break
-            case 3: 
-                threeValue += 1
-                break
-            case 4:
-                fourValue += 1
-                break
-            case 5:
-                fiveValue += 1
-                break
-        } 
-        if (nrOfSame(result) == number) {
-            clearInterval(interval)
-        }
-        rollValue += 1
-        updateHTMLfromValues()
-    },1) 
-}
-
-function allSame(result) {
-    for (let i = 1; i < result.length; i++) {
-        if (result[i] != result[0]) {return false}
-    }
-    return true
 }
 
 function nrOfSame(result) {
@@ -215,20 +272,73 @@ function highestInArray(array) {
     return output
 }
 
-function rollAllDicesAnimated(time) {
-    for (let i = 0; i < dices.length; i++) {
-    rollDiceAnimated(i, time)
-}}
-
 function initialize() {
 for (let i = 0; i < dices.length; i++) {
     let firstDiceFace = Math.floor(Math.random() * 6) + 1
     let dice = dices[i]
     dice.src = dice.src.slice(0, dice.src.length - 5) + `${firstDiceFace}` + ".png"
-    dice.addEventListener('click', (event) => rollDiceAnimated(event.target.id,5)
-    )
-    dice.addEventListener('dblclick', () => rollAllDicesAnimated(5))
 }}
+
+function outcome(result) {
+    if (isYatzy(result)) {return "5"}
+    else if (isSmallStraight(result)) {return "ss"}
+    else if (isLargeStraight(result)) {return "ls"}
+    else if (isFullHouse(result)) {return "fh"}
+    else if (isFour(result)) {return "4"}
+    else if (isThree(result)) {return "3"}
+    else if (isTwoPair(result)) {return "2p"}
+    else if (isTwo(result)) {return "2"}
+    else {return "n"}
+}
+
+function equalArrays(array1, array2) {
+    for (i = 0; i < array1.length; i ++) {
+        if (array1[i] != array2[i]) {return false}
+    }
+    return true
+}
+
+function isSmallStraight(result) {
+    return (equalArrays(result.sort(), [1,2,3,4,5]))
+}
+
+function isLargeStraight(result) {
+    return (equalArrays(result.sort(), [2,3,4,5,6]))
+}
+
+function nrOfValues(result) {
+    var values = [result[0]]
+    for (i = 1; i < result.length; i++) {
+        if (!values.includes(result[i])) {
+            values.push(result[i])
+        }
+    }
+    return values.length
+}
+
+function isFullHouse(result) {
+    return ((nrOfSame(result) == 3) && (nrOfValues(result) == 2))
+}
+
+function isTwoPair(result) {
+    return ( (nrOfSame(result) == 2) && (nrOfValues(result) == 3))
+}
+
+function isTwo(result) {
+    return ( (nrOfSame(result) == 2) && (nrOfValues(result) == 4))
+}
+
+function isFour(result) {
+    return (nrOfSame(result) == 4)
+}
+
+function isThree(result) {
+    return (nrOfSame(result) == 3)
+}
+
+function isYatzy(result) {
+    return (nrOfSame(result) == 5)
+}
 
 function rollDice() { 
     return Math.floor(Math.random() * 6) + 1
